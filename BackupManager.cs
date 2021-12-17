c2bde14f587a575c300ee197eae768ddb93e340f
Mateusz Tomanek <mateusz.tomanek@gmail.com>
2021-12-17 18:01:08 +0100

    Changed backup type, to zipped directories.

--- BackupManager.cs
+++ BackupManager.cs
@@ -1,37 +1,56 @@
-﻿using static GothicSaveBackupper.Helpers;
+﻿using System.Configuration;
+using System.IO.Compression;
+using static GothicSaveBackupper.Helpers;
 namespace GothicSaveBackupper;
 public class BackupManager
 {
     public FileSystemWatcher FileSystemWatcher { get; init; }
+
+    private readonly string[] IGNORE_DIRECTORIES = new string[] { "ref", "runtimes", "Backups" };
+
+    private string safeDateTime => $"{DateTime.Now:dd MM yyyy HH mm ss}";
     private DateTime _lastBackupTime = DateTime.Today;
     private string _backupFolderName = "Backups";
-    private List<string> filesToCopy;
+    private List<string> _directories;
+    private int _backupDelay = 10;
+
     public BackupManager(string backupFolderName)
     {
         _backupFolderName = backupFolderName;
 
-        var fsw = new FileSystemWatcher(AppDomain.CurrentDomain.BaseDirectory);
-        fsw.Changed += new FileSystemEventHandler(fsw_Changed);
-        fsw.Created += new FileSystemEventHandler(fsw_Changed);
-        fsw.Renamed += new RenamedEventHandler(fsw_Renamed);
-        fsw.Deleted += new FileSystemEventHandler(fsw_Deleted);
+        try
+        {
+            _backupDelay = Convert.ToInt32(ConfigurationManager.AppSettings["backupDelayTimeInSeconds"].ToString());
+        }
+        catch
+        {
+            _backupDelay = 10;
+        }
 
-        filesToCopy = new();
+        FileSystemWatcher = new(AppDomain.CurrentDomain.BaseDirectory);
+        EnableFileWatcher();
+
+        _directories = new();
+
         DiscoverFiles();
-
-        if (filesToCopy.Count > 0)
 
         ConsoleWriteLine("File system watcher created.");
         CreateBackupFolder();
-        FileSystemWatcher = fsw;
     }
 
     private void DiscoverFiles()
     {
-        foreach (var file in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory,"*.SAV"))
+        foreach (var directory in Directory.GetDirectories(Directory.GetCurrentDirectory()))
         {
-            filesToCopy.Add(file);
-            ConsoleWriteLine($"[Discovered file]: {Path.GetFileNameWithoutExtension(file)}", ConsoleColor.DarkCyan);
+            var dirName = Path.GetFileName(directory);
+            if (IGNORE_DIRECTORIES.Contains(dirName))
+                continue;
+
+            _directories.Add(directory);
+            ConsoleWriteLine($"[Discovered Directory]: {Path.GetFileNameWithoutExtension(directory)}", ConsoleColor.Cyan);
+
+            foreach (var file in Directory.GetFiles(directory, "*.SAV"))
+                ConsoleWriteLine($"[Discovered File]: {Path.GetFileNameWithoutExtension(file)}", ConsoleColor.DarkCyan);
         }
     }
 
@@ -45,6 +64,8 @@ public class BackupManager
 
     async void fsw_Changed(object sender, FileSystemEventArgs e)
     {
+        DisableFileWatcher();
+
         await Task.Delay(250);
 
         FileInfo fi = new FileInfo(e.FullPath);
@@ -52,15 +73,72 @@ public class BackupManager
         if (e.Name.Equals(_backupFolderName) && (e.ChangeType == WatcherChangeTypes.Deleted || e.ChangeType == WatcherChangeTypes.Renamed))
             FixBackupFolder();
 
-        if (fi is null || string.IsNullOrEmpty(fi.Extension))
+        if (fi is null)
+        {
+            EnableFileWatcher();
             return;
+        }
 
-        if ((DateTime.Now - _lastBackupTime).TotalSeconds < 3)
+        if (string.IsNullOrEmpty(fi.Extension) && e.ChangeType != WatcherChangeTypes.Deleted)
+        {
+            if (_directories.Contains(e.FullPath) == false | Directory.GetFiles(e.FullPath).Count() == 0)
+            {
+                EnableFileWatcher();
+                return;
+            }
+
+            BackupDirectory(e, fi);
+        }
+
+        EnableFileWatcher();
+    }
+    private void fsw_Created(object sender, FileSystemEventArgs e)
+    {
+        FileInfo fi = new FileInfo(e.FullPath);
+
+        if (string.IsNullOrEmpty(fi.Extension))
+        {
+            _directories.Add(e.FullPath);
+            ConsoleWriteLine($"[Discovered Directory]: {Path.GetFileNameWithoutExtension(e.FullPath)}", ConsoleColor.Cyan);
+        }
+    }
+
+    private void DisableFileWatcher()
+    {
+        FileSystemWatcher.Changed -= new FileSystemEventHandler(fsw_Changed);
+        FileSystemWatcher.Created -= new FileSystemEventHandler(fsw_Created);
+        FileSystemWatcher.Renamed -= new RenamedEventHandler(fsw_Renamed);
+        FileSystemWatcher.Deleted -= new FileSystemEventHandler(fsw_Deleted);
+    }
+
+    private void EnableFileWatcher()
+    {
+        FileSystemWatcher.Changed += new FileSystemEventHandler(fsw_Changed);
+        FileSystemWatcher.Created += new FileSystemEventHandler(fsw_Created);
+        FileSystemWatcher.Renamed += new RenamedEventHandler(fsw_Renamed);
+        FileSystemWatcher.Deleted += new FileSystemEventHandler(fsw_Deleted);
+    }
+
+    private async void BackupDirectory(FileSystemEventArgs e, FileInfo fi)
+    {
+        ConsoleWriteLine($"[Backup Begin]: Save change has been detected. Backup in {_backupDelay} seconds...", ConsoleColor.Yellow);
+        await Task.Delay(_backupDelay * 1000);
+        var zipName = $@"{fi.Directory}\{_backupFolderName}\{fi.Name} {safeDateTime}.zip";
+
+        ZipFile.CreateFromDirectory(fi.FullName, $@"{zipName}", CompressionLevel.Fastest, false);
+
+        if (File.Exists(zipName) == false)
+        {
+            ConsoleWriteLine($"[Backup Failed]: '{zipName}' doesn't exist.", ConsoleColor.Red);
             return;
+        }
 
+        ConsoleWriteLine($"[Backup created]: {zipName}", ConsoleColor.Green);
+    }
+
+    private async void BackupFile(FileSystemEventArgs e, FileInfo fi)
+    {
         ConsoleWriteLine($"[File {e.ChangeType}] {e.FullPath}");
-
-        _lastBackupTime = DateTime.Now;
         await CopyFileToBackupFolder(fi);
     }
 
@@ -75,18 +153,31 @@ public class BackupManager
         if (e.OldName.Equals(_backupFolderName) && (e.ChangeType == WatcherChangeTypes.Deleted || e.ChangeType == WatcherChangeTypes.Renamed))
             FixBackupFolder();
         else
-            ConsoleWriteLine($"[File {e.ChangeType}] {e.FullPath}{Environment.NewLine}Renamed files are not being backed up.", ConsoleColor.DarkMagenta);
+        {
+            if (_directories.Contains(Path.GetFileName(e.OldName)))
+                _directories.Remove(Path.GetFileName(e.OldName));
+
+            ConsoleWriteLine($"[File {e.ChangeType}] {e.FullPath}{Environment.NewLine}Renamed mapped directory.", ConsoleColor.DarkMagenta);
+
+            _directories.Add(e.Name);
+            ConsoleWriteLine($"[Discovered Directory]: {Path.GetFileNameWithoutExtension(e.FullPath)}", ConsoleColor.Cyan);
+        }
     }
 
-    async Task<bool> CopyFileToBackupFolder(FileInfo fileInfo)
+    async Task<bool> CopyFileToBackupFolder(FileInfo fileInfo, bool deleteOriginal = false)
     {
         var fileName = Path.GetFileNameWithoutExtension(fileInfo.Name);
         await Task.Delay(500);
         try
         {
-            var backupFileName = $"{_backupFolderName}\\{fileName} {DateTime.Now:dd mm yyyy HH mm ss}{fileInfo.Extension}";
-            File.Copy(fileInfo.Name, backupFileName);
-            ConsoleWriteLine($"[Backup created]: {backupFileName}", ConsoleColor.Green);
+            var backupFileName = $"{_backupFolderName}\\{fileName} {safeDateTime}{fileInfo.Extension}";
+
+            File.Copy(fileInfo.Name, backupFileName, true);
+
+            if (deleteOriginal)
+                File.Delete(fileInfo.Name);
+
+            ConsoleWriteLine($"[Backup Created]: {backupFileName}", ConsoleColor.Green);
             return true;
         }
         catch (IOException iex)
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## English
 
 #### How to use:
-- Copy GothicSaveBackupper.exe to saves folder of Gothic (or any other game)
+- Copy 'GothicSaveBackupper.exe' and 'GothicSaveBackupper.dll.config.xml' to saves folder of Gothic
+	- For example: `D:\Steam\steamapps\common\TheChroniclesOfMyrtana\saves_thechroniclesofmyrtana`
+- Setup your desired backup time delay after saving a file. If saving a game takes a long time on your machine, set the value way higher or your backup will be corrupted.
+	- Open file 'GothicSaveBackupper.dll.config.xml' in a editor of choice and change `value="x"`, where X is the number of seconds you see the save file panel + few seconds
 - Start GothicSaveBackupper.exe
 - Enjoy the game. With evry time you save a game this app will copy new file to \Backups\ folder
 
@@ -9,6 +12,9 @@
 ## Polski
 
 #### Jak używać:
-- Skopiuj GothicSaveBackupper.exe do folderu zawierającym save'y z Gothica (lub innej gry)
+- Skopiuj 'GothicSaveBackupper.exe' oraz 'GothicSaveBackupper.dll.config.xml' do folderu zawierającym save'y z Gothica
+	- Dla przykładu: `D:\Steam\steamapps\common\TheChroniclesOfMyrtana\saves_thechroniclesofmyrtana`
+- Ustaw wartość opóźnienia kopii podczas zapisywania w pliku 'GothicSaveBackupper.dll.config.xml'. Jeżeli gra zapisuje Ci się długo, kopia może wykonać się za szybko
+	- Otwórz plik w notatniku lub innym edytorze tekstu i zmień wartość `value="x"` gdzie X to ilość sekund, po których podczas zapisu wykona się kopia. Najlepiej wprowadzić tu rzeczywistą wartość ekranu zapisu + kilka sekund.
 - Uruchom GothicSaveBackupper.exe
 - Idź grać, przy każdej zmianie pliku program automatycznie przerzuci go do folderu \Backups\
